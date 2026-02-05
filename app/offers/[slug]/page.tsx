fbe56a8befaea7c78cf77368bb18a34ee663c4ca
Kajal Kotak <kajal.thobhani11@gmail.com>
2026-02-05 18:23:38 +0530

    Enable SSR for dynamic offers routes

--- app/offers/[slug]/page.tsx
+++ app/offers/[slug]/page.tsx
@@ -4,7 +4,7 @@ import { notFound } from "next/navigation";
 import Link from "next/link";
 import { packages } from "@/app/data/packages";
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
 /* ----------------------------------
    REQUIRED FOR VERCEL STATIC BUILD
@@ -12,11 +12,11 @@ export const dynamic = "force-dynamic";
 
 // export const dynamicParams = false;
 
-// export async function generateStaticParams() {
-//   return packages.map((pkg) => ({
-//     slug: pkg.slug,
-//   }));
-// }
+export async function generateStaticParams() {
+  return packages.map((pkg) => ({
+    slug: pkg.slug,
+  }));
+}
 
 /* ----------------------------------
    PAGE COMPONENT
